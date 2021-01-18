067465cc99f5121ed0382312920932349e7152fd
Frank Moreno <frankmoreno1993@gmail.com>
2021-01-17 19:50:37 -0500

    chore: app base

---
+++ vue-gql-swapi/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="appbar">
+    Ravn Star Wars Registry
+  </div>
+  <home-page>
+</template>
+
+<script>
+import HomePage from './components/HomePage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HomePage
+  }
+}
+</script>
+
+<style scoped>
+.appbar {
+  background-color: var(--rv-black);
+  color: white;
+  text-align: left;
+  padding: 16px 33px 16px 33px;
+  font-size: 17;
+  font-weight: bold;
+}
+</style>
