a20a03e3bc61ac202442e9543db1c1b04da8f402
Frank Moreno <frankmoreno1993@gmail.com>
2021-01-20 06:23:36 -0500

    chore: PersonCell

---
+++ vue-graphql-swapi/src/components/organisms/PersonCell.vue
@@ -0,0 +1,28 @@
+<template>
+  <list-item-cell
+    :title="person.name"
+    :subtitle="subtitle"
+  />
+</template>
+
+<script>
+import ListItemCell from '../molecules/ListItemCell.vue'
+
+export default {
+  components: {
+    ListItemCell
+  },
+  props: {
+    person: {
+      type: Object,
+      required: true,
+    }
+  },
+  computed: {
+    subtitle() {
+      const p = this.person
+      return `${p.species?.name ?? 'Human'} from ${p.homeworld.name}`
+    },
+  }
+}
+</script>
