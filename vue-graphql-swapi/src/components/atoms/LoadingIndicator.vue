78535140bf3c21e67aef58229a55e99cb8e58254
Frank Moreno <frankmoreno1993@gmail.com>
2021-01-20 06:08:18 -0500

    chore: Loading Indicator

---
+++ vue-graphql-swapi/src/components/atoms/LoadingIndicator.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="row">
+    <div>
+      <div class="loader"></div>
+    </div>
+    <h2 class="lowEmphasis" style="padding-left: 8px;">Loading</h2>
+  </div>
+</template>
+
+<style scoped>
+.row {
+  display: flex;
+}
+
+.loader {
+  border: 6px solid #f3f3f3;
+  border-top: 6px solid var(--rv-light);
+  border-radius: 50%;
+  width: 12px;
+  height: 12px;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+</style>
