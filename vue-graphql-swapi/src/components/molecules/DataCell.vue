e9088e5b7cadb793c245237ad7f0d01394b8905e
Frank Moreno <frankmoreno1993@gmail.com>
2021-01-20 06:14:09 -0500

    chore: DataCell

---
+++ vue-graphql-swapi/src/components/molecules/DataCell.vue
@@ -0,0 +1,34 @@
+<template>
+  <cell show-divider>
+    <div class="row">
+      <h2 class="lowEmphasis">{{ category }}</h2>
+      <h2>{{ value }}</h2>
+    </div>
+  </cell>
+</template>
+
+<script>
+import Cell from '../atoms/Cell'
+
+export default {
+  components: { Cell },
+  props: {
+    category: {
+      type: String,
+      require: true,
+    },
+    value: {
+      type: String,
+      require: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+.row {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+</style>
