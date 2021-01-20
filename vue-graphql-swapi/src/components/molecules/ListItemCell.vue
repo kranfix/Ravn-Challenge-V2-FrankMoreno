682f25449794803183b708d38f72d87dc9cc4b48
Frank Moreno <frankmoreno1993@gmail.com>
2021-01-20 06:18:58 -0500

    chore:  List Item Cell

---
+++ vue-graphql-swapi/src/components/molecules/ListItemCell.vue
@@ -0,0 +1,39 @@
+<template>
+  <cell text-align="left" :show-divider="true">
+    <h2>{{ title }}</h2>
+    <p class="lowEmphasis">{{ subtitle }}</p>
+    
+    <template v-slot:trailing>
+      <div>
+        <i class="material-icons icon-button">chevron_right</i>
+      </div>
+    </template>
+  </cell>
+</template>
+
+<script>
+import Cell from '../atoms/Cell.vue'
+
+export default {
+  components: {
+    Cell,
+  },
+  props: {
+    title: {
+      type: String,
+      require: true,
+    },
+    subtitle: {
+      type: String,
+      require: true,
+    },
+  }
+}
+</script>
+
+<style>
+.icon-button {
+  padding-top: 36px;
+  padding-right: 36px;
+}
+</style>
